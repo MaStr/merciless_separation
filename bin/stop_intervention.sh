11237a4f03e5d601ece1a8f82b63bef9cdd2e7a7
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-06-09 21:15:30 +0200

    Fix overlapping tun devices in Firewall rule

--- bin/stop_intervention.sh
+++ bin/stop_intervention.sh
@@ -2,4 +2,4 @@
 
 /etc/init.d/privoxy stop
 
-sudo iptables -t nat -D PREROUTING -i tun0 -p tcp --dport 80 -j REDIRECT --to-ports 8118
+sudo iptables -t nat -D PREROUTING -i ovpn-tun  -p tcp --dport 80 -j REDIRECT --to-ports 8118
