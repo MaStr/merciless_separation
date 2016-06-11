481c478aec51457fc2dd39b03291bb12f771b6ab
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-06-11 10:55:26 +0200

    Add HTTPS Firewall rules

--- bin/start_intervention.sh
+++ bin/start_intervention.sh
@@ -3,3 +3,6 @@
 /etc/init.d/privoxy start
 
 sudo iptables -t nat -I PREROUTING -i ovpn-tun  -p tcp --dport 80 -j REDIRECT --to-ports 8118
+
+#Disable all HTTPS
+sudo iptables -I FORWARD -p tcp --dport 443 -j REJECT
