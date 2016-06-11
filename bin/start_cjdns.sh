bf0eda39eb467794e178e40d955b404352ac0300
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-06-11 20:16:32 +0200

    Monit / CJDNS: adjustments  in startup scripts

--- bin/start_cjdns.sh
+++ bin/start_cjdns.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+
+
 config="/etc/cjdroute.conf"
 
+# FOR SOME REASON this has no effect , it seems
+#  I'm running a version that does not take account the piping
 test -e "/opt/cjdns/cjdroute.conf" && config="/opt/cjdns/cjdroute.conf"
 
 echo "Starting CJDNS with $config"
