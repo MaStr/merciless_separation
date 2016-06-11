a3a8c229eff71af421b027216c721b89f1090afc
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-06-11 19:06:08 +0200

    Monit: CJDNS start with config name output

--- bin/start_cjdns.sh
+++ bin/start_cjdns.sh
@@ -4,6 +4,7 @@ config="/etc/cjdroute.conf"
 
 test -e "/opt/cjdns/cjdroute.conf" && config="/opt/cjdns/cjdroute.conf"
 
+echo "Starting CJDNS with $config"
 cjdroute < "$config"
 sleep 1
 pgrep  cjdroute > /var/run/cjdns.pid
