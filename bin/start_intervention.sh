4091fe59a5f7d16b968a0b7724258b7019930218
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-06-24 00:35:07 +0200

    No HTTPS blocking on lab

--- bin/start_intervention.sh
+++ bin/start_intervention.sh
@@ -5,4 +5,4 @@
 sudo iptables -t nat -I PREROUTING -i ovpn-tun  -p tcp --dport 80 -j REDIRECT --to-ports 8118
 
 #Disable all HTTPS
-sudo iptables -I FORWARD -p tcp --dport 443 -j REJECT
+#sudo iptables -I FORWARD -p tcp --dport 443 -j REJECT
--- bin/stop_intervention.sh
+++ bin/stop_intervention.sh
@@ -4,4 +4,4 @@
 
 sudo iptables -t nat -D PREROUTING -i ovpn-tun  -p tcp --dport 80 -j REDIRECT --to-ports 8118
 
-sudo iptables -D FORWARD -p tcp --dport 443 -j REJECT
+#sudo iptables -D FORWARD -p tcp --dport 443 -j REJECT
