8dcb9bd343aaf5ea60dd6381973e8171dde4e847
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-06-28 19:35:17 +0200

    start intervention: Avoid dublicate iptables entries

--- bin/start_intervention.sh
+++ bin/start_intervention.sh
@@ -2,7 +2,16 @@
 
 /etc/init.d/privoxy start
 
-sudo iptables -t nat -I PREROUTING -i ovpn-tun  -p tcp --dport 80 -j REDIRECT --to-ports 8118
+
+if ! sudo iptables -t nat -L | grep -q -e "tcp dpt:http redir ports 8118" ; then 
+    sudo iptables -t nat -I PREROUTING -i ovpn-tun  -p tcp --dport 80 -j REDIRECT --to-ports 8118
+else
+    echo "iptables transparent HTTP rule existing, skipping."
+fi
 
 #Disable all HTTPS
-sudo iptables -I FORWARD -p tcp --dport 443 -j REJECT
+if ! sudo iptables -L FORWARD | grep -q -e "tcp dpt:https" ; then 
+	sudo iptables -I FORWARD -p tcp --dport 443 -j REJECT
+else
+    echo "iptables block FORWARD HTTPS  existing, skipping."
+fi
