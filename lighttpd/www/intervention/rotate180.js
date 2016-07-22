76eaa2678a04c7d71cd92d8c23117aea5b7a8bcc
Matthias Strubel <matthias.strubel@aod-rpg.de>
2016-07-22 15:43:22 +0200

    Add Intervention JS & CSS for flipping pages

---
+++ lighttpd/www/intervention/rotate180.js
@@ -0,0 +1,4 @@
+window.onload = scrollDownToTheTop;
+function scrollDownToTheTop() {
+    window.scrollTo(0, document.body.scrollHeight );
+}
---
+++ lighttpd/www/intervention/rotate90.js
@@ -0,0 +1,4 @@
+window.onload = scrollDownToTheTop;
+function scrollDownToTheTop() {
+    window.scrollTo(document.body.scrollWidth, document.body.scrollHeight / 2 );
+}
